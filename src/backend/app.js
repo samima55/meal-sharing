1f4f55b840f8c91661d1e9453effe83c58f70d9c
Samima Hassan <samimahassan55@gmail.com>
2023-07-04 10:39:30 +0200

    nodejs meal sharing week 1

--- src/backend/app.js
+++ src/backend/app.js
@@ -21,6 +21,78 @@ app.use(cors());
 
 router.use("/meals", mealsRouter);
 
+
+// Route to get future meals sorted by ID
+app.get("/future-meals", async (req, res) => {
+  try {
+    const meals = await knex
+      .select()
+      .from("meal")
+      .where("when", ">", knex.fn.now());
+    res.status(200).json(meals);
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
+});
+
+// Respond with all meals in the past (relative to the when datetime)
+app.get("/past-meals", async (req, res) => {
+  try {
+    const meals = await knex
+      .select()
+      .from("meal")
+      .where("when", "<", knex.fn.now())
+      .orderBy("id");
+    res.status(200).json(meals);
+  } catch (error) {
+    res.status(500).json(" Server Error");
+  }
+});
+
+
+// Respond with all meals sorted by ID
+app.get("/all-meals", async (req, res) => {
+  try {
+    const meals = await knex
+      .select("title", "description")
+      .from("meal")
+      .orderBy("id");
+    res.status(200).json(meals);
+  } catch (error) {
+    res.status(500).json("500. Internal Server Error");
+  }
+});
+
+
+//Respond with the first meal 
+app.get("/first-meal", async (req, res) => {
+  try {
+    const meal = await knex
+      .select("title", "description")
+      .from("meal")
+      .orderBy("id","asc")
+      .limit(1)
+    res.status(200).json(meal);
+  } catch (error) {
+    res.status(500).json("404 error");
+  }
+});
+
+
+//Respond with the last meal
+app.get("/last-meal", async (req, res) => {
+  try {
+    const meal = await knex
+      .select("title", "description")
+      .from("meal")
+      .orderBy("id", "desc")
+      .limit(1);
+    res.status(200).json(meal);
+  } catch (error) {
+    res.status(500).json("500. Internal Server Error");
+  }
+});
+
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
 } else {
